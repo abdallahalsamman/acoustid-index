04fc7518b896694dae28dc787f564b65a0ecda43
Ubuntu <ubuntu@ip-172-31-18-32.eu-central-1.compute.internal>
2024-01-29 21:18:19 +0000

    update

--- start.sh
+++ start.sh
@@ -4,6 +4,6 @@ rm -rf swteat_k
 mkdir alkulife
 mkdir doros_alkulify
 mkdir swteat_k
-./fpi-server -d alkulife &
+./fpi-server -d alkulife --http-port 6081 &
 ./fpi-server -d doros_alkulify --http-port 6083 &
 ./fpi-server -d swteat_k --http-port 6084
