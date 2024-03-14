c0d7b31721857bd611ccabb60127cf92c058777a
abdallah alsamman <sammanabdallah@gmail.com>
2024-03-14 05:45:46 +0000

    update

--- start.sh
+++ start.sh
@@ -1,9 +1,13 @@
+pkill -f fpi-server
 rm -rf alkulife
 rm -rf doros_alkulify
 rm -rf swteat_k
 mkdir alkulife
 mkdir doros_alkulify
 mkdir swteat_k
+rm alkulife/*
+rm doros_alkulify/*
+rm swteat_k/*
 ./fpi-server -d alkulife --http-port 6081 &
 ./fpi-server -d doros_alkulify --http-port 6083 &
 ./fpi-server -d swteat_k --http-port 6084
